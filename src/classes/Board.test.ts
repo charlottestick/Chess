e2a587cd5120318efc1369849321bf255af3209d
Charlotte Stick <75076090+charlottestick@users.noreply.github.com>
2022-11-29 19:35:05 +0000

    Change coordinates to a type and update Board

--- src/classes/Board.test.ts
+++ src/classes/Board.test.ts
@@ -1,26 +1,40 @@
 import { Board } from './Board';
+import { Coordinate } from '../types/Coordinate';
 
 describe('Board class', () => {
     describe('Given we instantiate the class', () => {
         let newBoard: Board;
-        beforeEach(() => {
+        beforeAll(() => {
             newBoard = new Board();
-        })
+        });
 
-        it('should create an intitial chess board 1D array', () => {
-          expect(newBoard).toHaveProperty('cells')
+        it('should create an initial chess board array', () => {
+            expect(newBoard).toHaveProperty('squares');
         });
-        it('should contain string characters to represent pieces', () => {
-          expect(newBoard.cells).toEqual([
-              'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R',
-              'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
-              ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-              ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-              ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-              ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-              'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
-              'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'
-          ])
+        it('should be a 2D array with string characters to represent pieces', () => {
+            expect(newBoard.squares).toEqual([
+                ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+                ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+                ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+            ]);
         });
+        it('should store each row as a separate array', () => {
+            expect(newBoard.squares[0]).not.toBe(newBoard.squares[7]);
+        });
+
+        describe.each([[{ x: 0, y: 0 }, 'R']])(
+            'Given we call getSquareFromCoordinate with a cartesian coordinate',
+            (coordinate: Coordinate, expected: string) => {
+                let square: string = newBoard.getSquareFromCoordinate(coordinate);
+                it('should return the correct piece at that position', () => {
+                    expect(square).toBe(expected);
+                });
+            }
+        );
     });
 });
--- src/classes/Board.ts
+++ src/classes/Board.ts
@@ -1,18 +1,27 @@
+import { Coordinate } from '../types/Coordinate';
+import { arrayCopier } from '../helpers/arrayCopier';
+
 const kingRow: Array<string> = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'];
 const pawnRow: Array<string> = ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'];
 const emptyRow: Array<string> = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
 export class Board {
-    cells: Array<string>;
+    squares: Array<Array<string>>;
 
     constructor() {
-        this.cells = kingRow
-            .concat(pawnRow)
-            .concat(emptyRow)
-            .concat(emptyRow)
-            .concat(emptyRow)
-            .concat(emptyRow)
-            .concat(pawnRow)
-            .concat(kingRow);
+        this.squares = [
+            arrayCopier(kingRow),
+            arrayCopier(pawnRow),
+            arrayCopier(emptyRow),
+            arrayCopier(emptyRow),
+            arrayCopier(emptyRow),
+            arrayCopier(emptyRow),
+            arrayCopier(pawnRow),
+            arrayCopier(kingRow)
+        ];
+    }
+
+    public getSquareFromCoordinate(coord: Coordinate): string {
+        return this.squares[coord.x][coord.y];
     }
 }
