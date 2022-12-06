63aea4fed32a2914bc7ca9443b8275145ba30143
Charlotte Stick <75076090+charlottestick@users.noreply.github.com>
2022-12-06 22:17:25 +0000

    WIP Update Board tests with redesigned squares property

--- src/classes/Board.test.ts
+++ src/classes/Board.test.ts
@@ -1,5 +1,11 @@
 import { Board } from './Board';
 import { Coordinate } from '../types/Coordinate';
+import { Square } from '../types/Squares';
+import { Piece } from './Piece';
+
+function createSquareWithPiece(x: number, y: number): Square {
+    return new Square(new Piece(x, y));
+}
 
 describe('Board class', () => {
     describe('Given we instantiate the class', () => {
@@ -8,36 +14,28 @@ describe('Board class', () => {
             newBoard = new Board();
         });
 
-        it('should create an initial chess board array', () => {
-            expect(newBoard).toHaveProperty('squares');
+        it('should create an initial chess board 2D 8x8 array', () => {
+            expect(newBoard.squares).toEqual([[], [], [], [], [], [], [], []]);
+            expect(newBoard.squares[0]).toHaveLength(8);
         });
-        it('should be a 2D array with string characters to represent pieces', () => {
-            expect(newBoard.squares).toEqual([
-                ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-                ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-                ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
-            ]);
+        it('should store square objects in the rows', () => {
+            expect(newBoard.squares[0][0]).toBeInstanceOf(Square);
         });
         it('should store each row as a separate array', () => {
             expect(newBoard.squares[0]).not.toBe(newBoard.squares[7]);
         });
 
-        describe.each([[{ x: 0, y: 0 }, 'R']])(
+        describe.each([[{ x: 0, y: 0 }, createSquareWithPiece(0, 0)]])(
             'Given we call getSquareFromCoordinate with a cartesian coordinate',
-            (coordinate: Coordinate, expected: string) => {
-                let square: string;
+            (coordinate: Coordinate, expected: Square) => {
+                let square: Square;
                 beforeEach(() => {
                     newBoard = new Board();
-                    square = newBoard.getSquareFromCoordinate(coordinate);
+                    square = newBoard.getSquare(coordinate.x, coordinate.y);
                 });
 
                 it('should return the correct piece at that position', () => {
-                    expect(square).toBe(expected);
+                    expect(square).toEqual(expected);
                 });
             }
         );
