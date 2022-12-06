c7735b3ae258b996f21420179dfb281a9596e459
Charlotte Stick <75076090+charlottestick@users.noreply.github.com>
2022-12-06 21:56:05 +0000

    Update Piece tests with redesigned coordinate inputs

--- src/classes/Piece.test.ts
+++ src/classes/Piece.test.ts
@@ -2,9 +2,9 @@ import { Piece } from './Piece';
 
 describe('Piece class', () => {
     describe('Given we have a new piece', () => {
-        let newPiece: Piece = new Piece({ x: 0, y: 0 }); // Have to initialise here or it thinks it's not defined yet, don't know why it's not recognising beforeEach
+        let newPiece: Piece = new Piece(0, 0); // Have to initialise here, or it thinks it's not defined yet, don't know why it's not recognising beforeEach
         beforeEach(() => {
-            newPiece = new Piece({ x: 0, y: 0 }); // reinitialise before each test, more independence
+            newPiece = new Piece(0, 0); // reinitialise before each test, more independence
         });
 
         it('should have a position property', () => {
@@ -19,7 +19,7 @@ describe('Piece class', () => {
 
         describe('and we call move with a position', () => {
             beforeEach(() => {
-                newPiece.move({ x: 2, y: 3 });
+                newPiece.move(2, 3);
             });
             it('should be in the specified position', () => {
                 expect(newPiece.position).toEqual({ x: 2, y: 3 });
